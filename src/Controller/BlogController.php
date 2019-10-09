867bbe83383a3085405cfecf0e161b931a03606a
Noled <e.morlon@codeur.online>
2019-10-09 13:09:37 +0200

    Add Database

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,6 +7,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use ZipArchive;
+use App\Entity\FileTransfer;
+use Doctrine\ORM\EntityManagerInterface;
 
 class BlogController extends AbstractController
 {
@@ -26,53 +28,70 @@ class BlogController extends AbstractController
      */
     public function sendData(Request $request, \Swift_Mailer $mailer)
     {
-      // retrieves $_GET and $_POST variables respectively
-      $mailFrom = filter_var(trim($request->request->get('mail_from')), FILTER_SANITIZE_EMAIL);
-      $mailTo = filter_var(trim($request->request->get('mail_to')), FILTER_SANITIZE_EMAIL);
-      $files = $request->files->get('files');
-      $nbElements = count($files);
-      $tmpFiles = array();
+      // Create entity
+      $fileTransfer = new FileTransfer();
+      $fileTransfer->setMailFrom(filter_var(trim($request->request->get('mail_from')), FILTER_SANITIZE_EMAIL));
+      $fileTransfer->setNameFrom($request->request->get('name_from'));
+      $fileTransfer->setMailTo(filter_var(trim($request->request->get('mail_to')), FILTER_SANITIZE_EMAIL));
+      $fileTransfer->setNameTo($request->request->get('name_to'));
 
-      // Unique zip name
-      $idZip = uniqid('zip_');
 
-      // // Add files to images reporitory and create zip archive
-      // $zip = new ZipArchive;
-      // if ($zip->open('zip/'.$idZip.'.zip', ZipArchive::CREATE) === TRUE){
-      //   $i=1;
-      //   foreach($files as $file){
-      //     $idImage = uniqid('img_');
-      //     $name = $idImage.'.'.pathinfo($file->getClientOriginalName(),PATHINFO_EXTENSION);
-      //     $tmpFiles[$i] = $file->move('images', $name);
-      //
-      //     $zip->addFile('images/'.$name, $name);
-      //
-      //     $i++;
-      //   }
-      // }
-      // // All files are added, so close the zip file.
-      // $zip->close();
+      if($fileTransfer->getMailFrom() != false && $fileTransfer->getMailTo() != false){
+        $files = $request->files->get('files');
+        $nbElements = count($files);
+        $tmpFiles = array();
 
-      // Create the message
-      $message = (new \Swift_Message())
-        // Give the message a subject
-        ->setSubject('Your subject')
-        // Set the From address with an associative array
-        ->setFrom([$mailFrom])
-        // Set the To addresses with an associative array (setTo/setCc/setBcc)
-        ->setTo([$mailTo])
-        // Give it a body
-        ->setBody('Here is the message itself')
-        // And optionally an alternative body
-        ->addPart('<q>Here is the message itself</q>', 'text/html')
-        // Optionally add any attachments
-        // ->attach(\Swift_Attachment::fromPath($files))
-        ;
+        // Unique zip name
+        $idZip = uniqid('zip_');
 
-        $mailer->send($message);
+        $fileTransfer->setFileName($idZip);
 
-        return new Response(
-            '<html><body>mail From : '.$mailFrom.'<br>mail to : '.$mailTo.'<br></body></html>'
-        );
+        // // Add files to images reporitory and create zip archive
+        $zip = new ZipArchive;
+        if ($zip->open('zip/'.$idZip.'.zip', ZipArchive::CREATE) === TRUE){
+          $i=1;
+          foreach($files as $file){
+            $idImage = uniqid('img_');
+            $name = $idImage.'.'.pathinfo($file->getClientOriginalName(),PATHINFO_EXTENSION);
+            $tmpFiles[$i] = $file->move('images', $name);
+
+            $zip->addFile('images/'.$name, $name);
+
+            $i++;
+          }
+        }
+        // All files are added, so close the zip file.
+        $zip->close();
+
+
+        // Create the message
+        $message = (new \Swift_Message())
+          ->setSubject('Fichiers envoyés par ' . $fileTransfer->getNameFrom())
+          ->setFrom([$fileTransfer->getMailFrom()])
+          ->setTo([$fileTransfer->getMailTo()])
+          ->setBody(
+              $this->renderView('email/sendMail.html.twig', [
+                  'nomDestinataire' => $fileTransfer->getNameTo(),
+                  'nomAuteur' => $fileTransfer->getNameFrom(),
+                  'link' => $fileTransfer->getFileName()
+              ]),
+              'text/html'
+          )
+          ;
+
+          $mailer->send($message);
+
+
+          // Insert into DB
+          $transferRepo = $this->getDoctrine()->getManager();
+          $transferRepo->persist($fileTransfer);
+          $transferRepo->flush();
+
+          return true;
+        }
+        // Error
+        else {
+          return false;
+        }
     }
 }
