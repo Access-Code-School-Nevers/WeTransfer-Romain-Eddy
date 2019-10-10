80663738fa2649b2763761c73140e062163d0c79
Noled <e.morlon@codeur.online>
2019-10-10 16:04:16 +0200

    fix javascript

--- public/JS/script.js
+++ public/JS/script.js
@@ -91,8 +91,8 @@ function formValidated() {
       overlayTransfer.style.height = "50%";
       var tmp = JSON.parse(this.responseText); // Parse json to access variables
 
-      dowloadInput.value = tmp.link;
-      dowloadButton.href = "http://localhost/WeTransfer-Romain-Eddy/public" + tmp.link;
+      dowloadInput.value = "http://localhost/WeTransfer-Romain-Eddy/public" + tmp.link;
+      dowloadButton.href = tmp.link;
       form.reset();
 
     } else if (this.readyState == 4 && this.status == 500) {
